81e63e0f63d3261e41b2a288214b7c87d7f1107f
ntlong12345 <longnthe172278@fpt.edu.vn>
2024-11-20 23:46:45 +0700

    Update Breadcrumb.tsx

--- src/pages/Breadcrumb/Breadcrumb.tsx
+++ src/pages/Breadcrumb/Breadcrumb.tsx
@@ -1,25 +1,23 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './Breadcrumb.css';
 
 interface BreadcrumbProps {
-  paths: { name: string; path: string }[]; // Danh sách các đường dẫn
+  paths: { name: string; path: string }[];
 }
 
 const Breadcrumb: React.FC<BreadcrumbProps> = ({ paths }) => {
   return (
     <nav>
       <ul className="breadcrumb">
-        {paths.map((item, index) => (
+        {paths.map((path, index) => (
           <li key={index} className="breadcrumb-item">
-            {index !== paths.length - 1 ? (
-              <Link to={item.path} className="breadcrumb-link">
-                {item.name}
-              </Link>
+            {index < paths.length - 1 ? (
+              <Link to={path.path} className="breadcrumb-link" >{path.name}</Link>
             ) : (
-              <span>{item.name}</span> 
+              <span>{path.name}</span>
             )}
-            {index !== paths.length - 1 && <span className="breadcrumb-separator"> &gt; </span>} {/* Dấu phân cách */}
+            {index < paths.length - 1 && <span className="breadcrumb-separator"> &gt; </span>}
           </li>
         ))}
       </ul>
