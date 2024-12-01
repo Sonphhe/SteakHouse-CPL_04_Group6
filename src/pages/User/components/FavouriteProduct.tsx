48eded020df4b8d06136ef3de8558ab4c5737426
ntlong12345 <longnthe172278@fpt.edu.vn>
2024-12-01 22:13:57 +0700

    Update FavouriteProduct.tsx

--- src/pages/User/components/FavouriteProduct.tsx
+++ src/pages/User/components/FavouriteProduct.tsx
@@ -1,27 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import './FavouriteProduct.css';
 import { useSteakHouseContext } from '../../../hooks/useSteakHouseContext';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 const FavouriteProduct = () => {
   const [favouriteProducts, setFavouriteProducts] = useState<any[]>([]);
   const { currentAccount } = useSteakHouseContext()
+  const navigate = useNavigate();
   const userId = currentAccount?.id; // User ID tạm thời
   useEffect(() => {
     const fetchFavourites = async () => {
       try {
         const response = await fetch("http://localhost:9999/favourite");
         console.log("Response status:", response.status);
-  
+
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
-  
+
         const data = await response.json();
         console.log("Fetched data:", data);
-  
+
         // Kiểm tra userId
         const userFavourites = data.find((item: any) => item.userId === userId);
         console.log("User favourites:", userFavourites);
-  
+
         if (userFavourites) {
           setFavouriteProducts(userFavourites.favoriteItems);
         } else {
@@ -31,13 +33,15 @@ const FavouriteProduct = () => {
         console.error("Error fetching favourites:", error);
       }
     };
-  
+
     fetchFavourites();
   }, []);
-  
-  
-  
-  
+
+
+  const handleProductClick = (product: any) => {
+    navigate(`/productdetail/${product.productName}`, { state: { product } });
+  };
+
 
   return (
     <div className="favourite-product">
@@ -46,11 +50,14 @@ const FavouriteProduct = () => {
         <div className="favourite-list">
           {favouriteProducts.map((product) => (
             <div className="favourite-item" key={product.id}>
-              <img src={product.image} alt={product.productName} />
-              <h3>{product.productName}</h3>
-              <p>Price: ${product.productPrice}</p>
-              {/* <p>{product.description}</p> */}
+              <div onClick={() => handleProductClick(product)}>
+                <img src={product.image} alt={product.productName} />
+                <h3>{product.productName}</h3>
+                <p>Price: ${product.productPrice}</p>
+                {/* <p>{product.description}</p> */}
+              </div>
             </div>
+
           ))}
         </div>
       ) : (
