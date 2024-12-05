7844e46788feb8f07effd4a6659be8cd7d487752
LuongQuangTrungHE170149 <134850662+LuongQuangTrungHE170149@users.noreply.github.com>
2024-12-05 17:35:40 +0700

    Update FlashSaleCard

--- src/pages/Sale/FlashSaleComponents/FlashSaleCard.tsx
+++ src/pages/Sale/FlashSaleComponents/FlashSaleCard.tsx
@@ -16,14 +16,12 @@ const FlashSaleCard = ({
 
   // Ép kiểu `prod.id` về số trước khi so sánh
   const product = products.find((prod) => {
-    console.log(
-      `Comparing prod.id (${prod.id}, type: ${typeof prod.id}) with productId (${productId}, type: ${typeof productId})`
-    );
+    
     return Number(prod.id) === productId; // Ép kiểu `prod.id` về số
   });
 
   if (!product) {
-    console.error(`Product not found for Product ID: ${productId}`);
+    
     return (
       <div className="flash-sale-item">
         <h3>Product not found</h3>
@@ -43,12 +41,11 @@ const FlashSaleCard = ({
       <div onClick={() => handleProductClick(product)}>
       <img src={product.image} alt={product.productName} />
       <h3>{product.productName}</h3>
-      <p className="original-price">${product.productPrice.toFixed(2)}</p>
-      <p className="sale-price">${salePrice.toFixed(2)}</p>
+      <p className="original-price">${product.productPrice.toFixed(3)}đ</p>
+      <p className="sale-price">${salePrice.toFixed(3)}đ</p>
       <p className="sale-percent">-{sale}%</p>
       <p className="sale-timer">Ends on: {new Date(endDate).toLocaleDateString()}</p>
       </div>
-      
     </div>
   );
 };
